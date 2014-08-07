774aa36bd484ebce4757b73fea46e65675386d72
mathgenome <mathgenome@gmail.com>
2014-08-07 11:56:28 -0500

    First commit

---
+++ plot1.R
@@ -0,0 +1,27 @@
+#Exploratory Data Analysis
+#Project 1, Plot 1
+#Calling the data
+#Due to the size of the data set, rows will be selected for dates
+#2007-02-01 and 2007-02-02
+#This speeds up the process greatly
+#Loading package for sql
+library(sqldf)
+#Selecting for dates 01/02/2007 and 02/02/2007
+dat <- read.csv.sql("household_power_consumption.txt",head=TRUE,sep=";", sql = 'select * from file where Date = "1/2/2007" OR Date = "2/2/2007"')
+nrow(dat)
+#Check for missing data labeled as "?"
+length(which(dat=="?"))
+#Check for missing data labeled as NA
+length(which(is.na(dat)))
+#Change date format
+dat$Date<-as.Date(dat$Date,"%d/%m/%Y")
+#Generating a column with Date-Time information
+x<-paste(dat$Date,dat$Time)
+x<-strptime(x,"%Y-%m-%d %H:%M:%S")
+dat$DateTime<-x
+rm(x)
+#Plot 1
+png("plot1.png",width=480,height=480)
+par(bg="gray60")
+hist(dat$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,31 @@
+#Exploratory Data Analysis
+#Project 1, Plot 3
+#Calling the data
+#Due to the size of the data set, rows will be selected for dates
+#2007-02-01 and 2007-02-02
+#This speeds up the process greatly
+#Loading package for sql
+library(sqldf)
+#Selecting for dates 01/02/2007 and 02/02/2007
+dat <- read.csv.sql("household_power_consumption.txt",head=TRUE,sep=";", sql = 'select * from file where Date = "1/2/2007" OR Date = "2/2/2007"')
+nrow(dat)
+#Check for missing data labeled as "?"
+length(which(dat=="?"))
+#Check for missing data labeled as NA
+length(which(is.na(dat)))
+#Change date format
+dat$Date<-as.Date(dat$Date,"%d/%m/%Y")
+#Generating a column with Date-Time information
+x<-paste(dat$Date,dat$Time)
+x<-strptime(x,"%Y-%m-%d %H:%M:%S")
+dat$DateTime<-x
+rm(x)
+#Plot 3
+png("plot3.png",width=480,height=480)
+par(bg="gray60")
+plot(dat$DateTime,dat$Sub_metering_1,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+lines(dat$DateTime,dat$Sub_metering_2,col="red")
+lines(dat$DateTime,dat$Sub_metering_3,col="blue")
+legend("topright", col = c("black","red", "blue"), lty=c(1,1,1),legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
